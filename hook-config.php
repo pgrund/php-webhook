2662d27dbf5262d29f5151c58973bc2de7038bf5
Peter Grund <peter.grund@bisnode.de>
2014-07-04 08:18:21 +0200

    configuration via class

--- hook-config.php
+++ hook-config.php
@@ -2,6 +2,73 @@
 
 define("SECRET_TOKEN", "BetterChangeThisOneHere");
 define("ALLOWED_IPS", "94.45.140.46,207.97.227.253,50.57.128.197");//default github push ips
+
 define("ZIP_FILENAME", "latest.zip");
 define("EXTRACT_FOLDER", "latest");
+
+define("PROJECT_NAME", "My Project");
+
+
+class Config {
+
+    public $json;
+    private $errors = array(
+        400 => "Bad Request",
+        403 => "Not Authorized",
+        500 => "Internal Server Error"
+    );
+    private $allowed_ips;
+    private $secret;
+
+    function __construct() {
+        $this->secret = sha1(SECRET_TOKEN);
+        $this->allowed_ips = explode(",", ALLOWED_IPS);
+    }
+
+    function verify() {
+        // check IP
+        if (!(in_array($_SERVER["REMOTE_ADDR"], $this->allowed_ips))) {
+            $this->errorResponse(403);
+        }
+        // check signature
+        $signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];
+        if (!(isset($signature) && $signature === $this->secret)) {
+            $this->errorResponse(403);
+        }
+    }
+
+    /* helper function processing http error status message */
+
+    function errorResponse($code = 400, $msg = null) {
+        // statuscode handling
+        $header = $this->errors[$code];
+        if (!(isset($header))) {
+            $header = "";
+        }
+        header("HTTP/1.1 " . $code . " " . $header);
+
+        // if message is present, append as text/plain
+        if (isset($msg)) {
+            header("Content-Type: text/plain");
+            echo $msg;
+        }
+        die();
+    }
+
+    function checkJson() {
+        if (!isset($this->json, $this->json["release"], $this->json["release"]["zipball_url"])) {
+            $this->errorResponse(400, "wrong payload - github release event (application/json) expected");
+        }
+    }
+    
+    function remoteUrl() {
+        if(isset($this->json)) {
+            return $this->json["release"]["zipball_url"];
+        } else {
+            return null;
+        }
+    }
+
+}
+
 ?>
--- hook.php
+++ hook.php
@@ -1,69 +1,6 @@
 <?php
 
 require('hook-config.php');
-
-class Config {
-
-    public $json;
-    private $errors = array(
-        400 => "Bad Request",
-        403 => "Not Authorized",
-        500 => "Internal Server Error"
-    );
-    private $allowed_ips;
-    private $secret;
-
-    function __construct() {
-        $this->secret = sha1(SECRET_TOKEN);
-        $this->allowed_ips = explode(",", ALLOWED_IPS);
-    }
-
-    function verify() {
-        // check IP
-        if (!(in_array($_SERVER["REMOTE_ADDR"], $this->allowed_ips))) {
-            $this->errorResponse(403);
-        }
-        // check signature
-        $signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];
-        if (!(isset($signature) && $signature === $this->secret)) {
-            $this->errorResponse(403);
-        }
-    }
-
-    /* helper function processing http error status message */
-
-    function errorResponse($code = 400, $msg = null) {
-        // statuscode handling
-        $header = $this->errors[$code];
-        if (!(isset($header))) {
-            $header = "";
-        }
-        header("HTTP/1.1 " . $code . " " . $header);
-
-        // if message is present, append as text/plain
-        if (isset($msg)) {
-            header("Content-Type: text/plain");
-            echo $msg;
-        }
-        die();
-    }
-
-    function checkJson() {
-        if (!isset($this->json, $this->json["release"], $this->json["release"]["zipball_url"])) {
-            $this->errorResponse(400, "wrong payload - github release event (application/json) expected");
-        }
-    }
-    
-    function remoteUrl() {
-        if(isset($this->json)) {
-            return $this->json["release"]["zipball_url"];
-        } else {
-            return null;
-        }
-    }
-
-}
-
 $config = new Config();
 
 /* helper function for deleting a nonempty directory */
@@ -89,10 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // handle security
     $config->verify();
 
-    // get post data
+    // get post data and process json
     $input = file_get_contents('php://input');
     $config->json = json_decode($input, true);
-
     $config->checkJson();
 
     // copy remote zip file locally
@@ -100,14 +36,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $opts = array(
         'http' => array(
             'method' => "GET",
-            'user_agent' => "NWComamnds release update poller",
+            'user_agent' => PROJECT_NAME." release update poller",
             'max_redirects' => 20,
             'request_fulluri' => true
     ));
     if (!copy($file, ZIP_FILENAME, stream_context_create($opts))) {
         $config->errorResponse(500, "failed to copy stream to file ...");
+        
     } else {
-
         // extract zip file
         $zip = new ZipArchive;
         $res = $zip->open(ZIP_FILENAME, ZIPARCHIVE::CREATE);
@@ -130,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 echo "<html>
     <head>
-    <title>Latest release</title>
+    <title>Latest release for ".PROJECT_NAME."</title>
     </head>
      <body>
       <h1>Latest</h1>";
@@ -141,7 +77,7 @@ if (isset($config->json)) {
 }
 echo "
       <p>    
-      <a href='./".ZIP_FILENAME."' target='_blank'>Latest Release (Zip)</a> for download
+      <a href='./".ZIP_FILENAME."' target='_blank'>Latest Release of ".PROJECT_NAME."(Zip)</a> for download
       </p>
       <p>Please see <a href='./".EXTRACT_FOLDER."'>".EXTRACT_FOLDER."</a> for files ...</p>
      </body>
