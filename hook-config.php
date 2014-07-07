19f7482544c6ce3d5d2345da148cb8257df4199f
Peter Grund <peter.grund@bisnode.de>
2014-07-07 18:13:31 +0200

    renamed errorResponse, added comments

--- hook-config.php
+++ hook-config.php
@@ -1,18 +1,23 @@
 <?php
 
 define("SECRET_TOKEN", "BetterChangeThisOneHere");
-define("ALLOWED_IPS", "192.30.252.0/22"); // see https://help.github.com/articles/what-ip-addresses-does-github-use-that-i-should-whitelist
-//"94.45.140.46,207.97.227.253,50.57.128.197"  //==> old github push ips
+
+// see https://help.github.com/articles/what-ip-addresses-does-github-use-that-i-should-whitelist
+define("ALLOWED_IPS", "192.30.252.30/5"); 
+
 
 define("ZIP_FILENAME", "latest.zip");
 define("EXTRACT_FOLDER", "latest");
 
 define("PROJECT_NAME", "My Project");
 
-
+/**
+ * wrapping all configuration and access stuff in an own class.
+ */
 class Config {
 
     public $json;
+
     private $errors = array(
         400 => "Bad Request",
         403 => "Not Authorized",
@@ -38,40 +43,55 @@ class Config {
         }
     }
 
-    function verify($payload) {
+   /**
+    * do security checks.
+    *
+    * checking IP and signature header
+    */
+    private function verify($payload) {
         // check IP
         if (!(in_array($_SERVER["REMOTE_ADDR"], $this->allowed_ips))) {
-            $this->errorResponse(403);
-        }
-        // check signature
-        $signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];
-        if(!(isset($payload))||!(isset($signature))) {
-            $this->errorResponse(403, "missing payload");
+            $this->httpResponse(403);
         }
 
-        $secret = "sha1=".hash_hmac('sha1', $input, SECRET_TOKEN);
+        // check prerequisities
+        $signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];
+        if(!(isset($payload))||!(isset($signature))) {
+            $this->httpResponse(403);
+        }
+        //check signature
+        $secret = "sha1=".hash_hmac('sha1', $payload, SECRET_TOKEN);
         if ( $signature === $secret ) {
-            $this->errorResponse(403);
+            $this->httpResponse(403);
         }
     }
 
+   /**
+    * process payload.
+    *
+    * special handling for ping, parse json
+    */
     function setPayload($input) {
         $this->verify($input);
 
          // handle ping
         if($_SERVER["HTTP_X_GITHUB_EVENT"] === "ping") {
-            $this->errorResponse(200, "ping successfull (checks passed)");
+            $this->httpResponse(200, "ping successfull (checks passed)");
         }
 
         $this->json = json_decode($input, true);
         if (!isset($this->json, $this->json["release"], $this->json["release"]["zipball_url"])) {
-            $this->errorResponse(400, "wrong payload - github release event (application/json) expected");
+            $this->httpResponse(400, "wrong payload - github release event (application/json) expected");
         }        
     }
 
-    /* helper function processing http error status message */
-
-    function errorResponse($code = 400, $msg = null) {
+   /**
+    * return http statuscode.
+    *
+    * stops further processing, send stauscode directly
+    * default statuscode 400, optional message
+    */
+    function httpResponse($code = 400, $msg = null) {
         // statuscode handling
         $header = $this->errors[$code];
         if (!(isset($header))) {
@@ -87,6 +107,9 @@ class Config {
         die();
     }
    
+   /**
+    * get remote url for file download.
+    */
     function remoteUrl() {
         if(isset($this->json)) {
             return $this->json["release"]["zipball_url"];
